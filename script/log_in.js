3c9f7f3558e782a415288c8515797fbc2057a897
Nawey99 <Nawey99@gmail.com>
2023-02-17 10:12:26 +0300

    log in

--- script/log_in.js
+++ script/log_in.js
@@ -7,26 +7,26 @@ const labels=document.querySelectorAll('.form_control label')
             style="transition-delay: ${idx * 50}ms">${letter}</span>`)
             .join('')
           })
-          $(function() {
-            $( ".calendar" ).datepicker({
-              dateFormat: 'dd/mm/yy',
-              firstDay: 1
-            });
+          // $(function() {
+          //   $( ".calendar" ).datepicker({
+          //     dateFormat: 'dd/mm/yy',
+          //     firstDay: 1
+          //   });
             
-            $(document).on('click', '.date-picker .input', function(e){
-              var $me = $(this),
-                  $parent = $me.parents('.date-picker');
-              $parent.toggleClass('open');
-            });
+          //   $(document).on('click', '.date-picker .input', function(e){
+          //     var $me = $(this),
+          //         $parent = $me.parents('.date-picker');
+          //     $parent.toggleClass('open');
+          //   });
             
             
-            $(".calendar").on("change",function(){
-              var $me = $(this),
-                  $selected = $me.val(),
-                  $parent = $me.parents('.date-picker');
-              $parent.find('.result').children('span').html($selected);
-            });
-          });
+          //   $(".calendar").on("change",function(){
+          //     var $me = $(this),
+          //         $selected = $me.val(),
+          //         $parent = $me.parents('.date-picker');
+          //     $parent.find('.result').children('span').html($selected);
+          //   });
+          // });
            
           var email=document.forms['form'] ['email'];
           var password=document.forms['form'] ['password'];

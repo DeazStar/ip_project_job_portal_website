8f636e27a9388bd9435ba10948b2462fe65a3300
DeazStar <deadfromtheborn@gmail.com>
2023-03-24 01:21:31 +0300

    change the naviagation bar

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,7 +1,11 @@
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav')
+const hamburgerBtn = document.querySelector(".hamburger");
+const mobileNaviagtion = document.querySelector(".mobile-link-container");
+const closeBtn = document.querySelector(".close-btn");
 
-menu_btn.addEventListener('click', function() {
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+hamburgerBtn.addEventListener("click", () => {
+    mobileNaviagtion.classList.add("is-active");
 });
+
+closeBtn.addEventListener("click", () => {
+    mobileNaviagtion.classList.remove("is-active");
+})

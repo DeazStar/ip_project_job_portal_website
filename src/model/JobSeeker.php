6eaa37864c9ba6e09233aa7b400e356902d67e98
DeazStar <deadfromtheborn@gmail.com>
2023-05-11 22:41:00 +0300

    update JobSeeker

--- src/model/JobSeeker.php
+++ src/model/JobSeeker.php
@@ -5,20 +5,44 @@ require_once 'PersonalInfo.php';
 class JobSeeker {
     private int $id;
     private PersonalInfo $personalInfo;
+
+    /**
+     * jobSeeker construct 
+     * 
+     * Create a new instance of the jobSeeker class.
+     */
     public function __construct(int $id){
         $this->id = $id;
         $this->personalInfo = new PersonalInfo();
     }
 
+    /**
+     * getter method for the jobSeeker id
+     * 
+     */
     public function getId():int {
         return $this->id;
     }
 
+    /**
+     * setter method for the jobSeeker id
+     * 
+     * @param int $id
+     */
     public function setId(int $id):void {
         $this->id =$id;
     }
 
 
-    public function updateProfile(array $data) {
+    /**
+     * updateProfile: update personal information of the job seeker
+     * 
+     * @param array $data: the $_POST data
+     * 
+     * @return void
+     */
+    public function updateProfile(array $data):void {
+        $this->personalInfo->setFromArray($data);
+        $this->personalInfo->updatePersonalInfo($this->id);
     }
 }
